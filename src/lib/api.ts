49280a5b0a03e70e4bce0c224a2f97ab1509ab58
jeremi-24 <jeremiekoue8@gmail.com>
2025-06-26 00:11:20 +0000

    00h00

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@
 import type { Categorie, Produit } from './types';
 
 // All API calls will be sent to the Next.js proxy configured in next.config.ts
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'http://192.168.1.140';
 
 async function apiFetch(endpoint: string, options: RequestInit = {}) {
   const url = `${API_BASE_URL}${endpoint}`;
