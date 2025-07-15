0682634e2c2f9a6b9b9d3370ce0a47954877c134
ekoue jeremie <jeremiekoue8@gmail.com>
2025-07-15 10:51:14 +0000

    Update API destination in next.config.ts to local server

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,8 @@ const nextConfig: NextConfig = {
       {
         source: '/api/:path*',
         // Le proxy pour les appels côté client
-        destination: 'https://8080-firebase-stcokback-1751036275628.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/api/:path*',
+        //destination: 'https://8080-firebase-stcokback-1751036275628.cluster-l6vkdperq5ebaqo3qy4ksvoqom.cloudworkstations.dev/api/:path*',
+        destination: 'http://192.168.0.124:8080/api/:path*',
       },
     ]
   },
