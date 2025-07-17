78b08c522680c054519cd7b76ff550ef911e4343
jeremie Ekoue <bedjra12@gmail.com>
2025-07-17 23:25:55 +0000

    role de l'utilisateur est une chaîne de caractères ("ADMIN") et non un o

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -230,7 +230,7 @@ export type RoleCreationPayload = {
 export type CurrentUser = {
   id: number;
   email: string;
-  role: Role;
+  role: string;
   lieuId?: number;
   clientId?: number;
   permissions: Permission[];
