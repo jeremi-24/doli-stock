24f920714e66b9f83f7cd90eb4e54bbdf5bc570b
jeremi Ekoue <jeremiekoue8@gmail.com>
2025-07-29 09:14:04 +0000

    fix destination

--- next.config.ts
+++ next.config.ts
@@ -17,19 +17,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
-    // Vérifie si l'environnement est en développement
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://api.megatram.biz/api/:path*',
-        },
-      ];
-    }
-    // En production, pas de réécriture
-    return [];
-  },
+ 
 };
 
 export default nextConfig;
