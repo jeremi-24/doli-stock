dd4b0becd1302e51e0a5ff92c369e458d24fefd7
jeremi Ekoue <jeremiekoue8@gmail.com>
2025-08-27 11:00:00 +0000

    add domain source dolibar

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'sta-dolibarr.megatram.biz',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
  
